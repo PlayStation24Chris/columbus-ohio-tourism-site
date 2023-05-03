cd62615016e1f8e002201fef2aae20e9e0199272
unknown <PS24CKowalewski@efcts.us>
2023-05-03 08:10:07 -0400

    icon

--- map.js
+++ map.js
@@ -1,5 +1,5 @@
 //Initialize map
-let map = L.map('map').setView([39.960938, -83.017194], 19)
+let map = L.map('map').setView([39.960938, -83.017194], 19);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -7,4 +7,19 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+//create icon for markers
+
+let testIcon = L.icon({
+    iconUrl: '../Resources/Images/Bat Walter White.jpg',
+
+    iconSize: [25, 48],
+    shadowSize: [38, 40],
+    iconAnchor: [19, 30],
+    shadowAnchor: [4, 20],
+    popupAnchor: [-5, -40]
+});
+
+L.marker([39.950008, -82.823418], {icon: testIcon}).addTo(map);
+
+//NOTES
 //add marker at 39.950010 -82.823420
