34fb7cb6ec1853e5a8f49edc089754112306bc0d
AlmnoA <113357290+AlmnoA@users.noreply.github.com>
2023-05-09 10:14:05 -0400

    some?

--- map.js
+++ map.js
@@ -24,8 +24,6 @@ let testIcon = L.icon({
     popupAnchor: [-5, -40]
 });
 /*
-L.marker([39.964821, -82.978752], {icon: testIcon}).addTo(map);// art museum 
-L.marker([39.959277, -82.980705], {icon: testIcon}).addTo(map);//hospitlal
 L.marker([39.989479,-83.005341], {icon: testIcon}).addTo(map);//kroger
 L.marker([39.7389, -83.3441], {icon: testIcon}).addTo(map);//ohio is real
 // add popup
@@ -45,29 +43,13 @@ L.marker([39.7389, -83.3441], {icon: testIcon}).addTo(map);//ohio is real
 //READ AND PLACE LOCATIONS
 for (let j=0; j<locValues.length; ++j)
 {
-    let subString = "Yes!";
-    if (!locValues[j].Subway)
-    {
-        subString = "No."
-    }
-
     L.marker([locValues[j].Lat, locValues[j].Long], {icon: testIcon}).addTo(map).on('click', function(){
-        L.popup().setLatLng([locValues[j].Lat, locValues[j].Long]).setContent(locValues[j].FlavorText + "<br>Address: " + locValues[j].Address + "<br>Link: <a target=\"_blank\" href=\"" + locValues[j].Link + "\">Visit Here</a><br>Is this a subway? " + subString).openOn(map);
+        L.popup().setLatLng(
+            [locValues[j].Lat,locValues[j].Long])
+            .setContent("<div class=\"Pop-up \"><h1>"+locValues[j].Name+"</h1>" +"<h2>"+ locValues[j].FlavorText+"</h2>"+ "<p>Address: " + locValues[j].Address + "</p>Learn More!: <a  href=\"" + locValues[j].href + "\">Visit Here</a></div>").openOn(map);
     });
 }
 
 //NOTES
 //39.989479,-83.005341
 //add marker at 39.950010 -82.823420
-
-//SUBWAYS
-/*
-40.1179231, -83.0150081
-39.9589294, -82.996934
-39.9162279, -83.0358222
-40.0499474, -83.0516656
-39.9275356, -83.1203298
-40.0598008, -82.9861377
-40.0072525, -83.0463039
-39.9940551, -82.9811159
-*/
