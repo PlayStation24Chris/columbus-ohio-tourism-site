59286d83dd3d805134e87e95873a4f13cfc771ec
PlayStation24Chris <PS24CKowalewski@efcts.us>
2023-05-14 19:19:03 -0400

    major buff to firefox

--- map.js
+++ map.js
@@ -5,12 +5,6 @@
 //Initialize map
 let map = L.map("map").setView([39.960938, -83.017194], 11);
 
-//import data
-import localData from "../locations.json" assert { type: "json" };
-const locValues = Object.values(localData);
-
-import SubwayData from "../YelpSubway.json" assert { type: "json" };
-
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 19,
   minZoom: 11,
@@ -31,66 +25,88 @@ let testIcon = L.icon({
   popupAnchor: [-5, -40],
 });
 
-//READ AND PLACE LOCATIONS
-for (let j = 0; j < locValues.length; ++j) {
-  L.marker([locValues[j].Lat, locValues[j].Long], { icon: testIcon })
-    .addTo(map)
-    .on("click", function () {
-      L.popup()
-        .setLatLng([locValues[j].Lat, locValues[j].Long])
-        .setContent(
-          '<div class="Pop-up "><h1>' +
-            locValues[j].Name +
-            "</h1>" +
-            "<h2>" +
-            locValues[j].FlavorText +
-            "</h2>" +
-            "<p>Address: " +
-            locValues[j].Address +
-            '</p>Learn More!: <a  href="' +
-            locValues[j].href +
-            '">Click here</a></div>'
-        )
-        .openOn(map);
-    });
+loadJSON();
+
+//import data
+async function loadJSON()
+{
+  let response = await fetch('../locations.json');
+  const localData = await response.json();
+  
+  response = await fetch('../YelpSubway.json');
+  const SubwayData = await response.json();
+
+  fillMap(Object.values(localData), SubwayData);
 }
-//load the Sub Way
-for (let j = 0; j < SubwayData.businesses.length; ++j) {
-  testIcon = L.icon({
-    iconUrl: "../Resources/Images/marker.png",
-    iconSize: [25, 48],
-    shadowSize: [38, 40],
-    iconAnchor: [19, 30],
-    shadowAnchor: [4, 20],
-    popupAnchor: [-5, -40],
-  });
-  L.marker(
-    [
-      SubwayData.businesses[j].coordinates.latitude,
-      SubwayData.businesses[j].coordinates.longitude,
-    ],
-    { icon: testIcon }
-  )
-    .addTo(map)
-    .on("click", function () {
-      L.popup()
-        .setLatLng([
-          SubwayData.businesses[j].coordinates.latitude,
-          SubwayData.businesses[j].coordinates.longitude,
-        ])
-        .setContent(
-          '<div class="Pop-up "><h1>' +
-            SubwayData.businesses[j].name +
-            "</h1>" +
-            "<h2>" +
-            SubwayData.businesses[j].rating +
-            "/5 </h2>" +
-            "<p>Address: " +
-            SubwayData.businesses[j].location.address1 +
-            '</p>Learn More<a href="subway.html?address='+ SubwayData.businesses[j].location.display_address.join('_') +'">' +
-            " Click Here!" +
-            "</a></div>"
-        )
-        .openOn(map);
+
+// import localData from "../locations.json" assert { type: "json" };
+// const locValues = Object.values(localData);
+// import SubwayData from "../YelpSubway.json" assert { type: "json" };
+
+function fillMap(locValues, SubwayData)
+{
+  
+  //READ AND PLACE LOCATIONS
+  for (let j = 0; j < locValues.length; ++j) {
+    L.marker([locValues[j].Lat, locValues[j].Long], { icon: testIcon })
+      .addTo(map)
+      .on("click", function () {
+        L.popup()
+          .setLatLng([locValues[j].Lat, locValues[j].Long])
+          .setContent(
+            '<div class="Pop-up "><h1>' +
+              locValues[j].Name +
+              "</h1>" +
+              "<h2>" +
+              locValues[j].FlavorText +
+              "</h2>" +
+              "<p>Address: " +
+              locValues[j].Address +
+              '</p>Learn More!: <a  href="' +
+              locValues[j].href +
+              '">Click here</a></div>'
+          )
+          .openOn(map);
+      });
+  }
+  //load the Sub Way
+  for (let j = 0; j < SubwayData.businesses.length; ++j) {
+    testIcon = L.icon({
+      iconUrl: "../Resources/Images/marker.png",
+      iconSize: [25, 48],
+      shadowSize: [38, 40],
+      iconAnchor: [19, 30],
+      shadowAnchor: [4, 20],
+      popupAnchor: [-5, -40],
     });
+    L.marker(
+      [
+        SubwayData.businesses[j].coordinates.latitude,
+        SubwayData.businesses[j].coordinates.longitude,
+      ],
+      { icon: testIcon }
+    )
+      .addTo(map)
+      .on("click", function () {
+        L.popup()
+          .setLatLng([
+            SubwayData.businesses[j].coordinates.latitude,
+            SubwayData.businesses[j].coordinates.longitude,
+          ])
+          .setContent(
+            '<div class="Pop-up "><h1>' +
+              SubwayData.businesses[j].name +
+              "</h1>" +
+              "<h2>" +
+              SubwayData.businesses[j].rating +
+              "/5 </h2>" +
+              "<p>Address: " +
+              SubwayData.businesses[j].location.address1 +
+              '</p>Learn More<a href="subway.html?address='+ SubwayData.businesses[j].location.display_address.join('_') +'">' +
+              " Click Here!" +
+              "</a></div>"
+          )
+          .openOn(map);
+      });
+  }
 }
