e377ae2519a2c2325b8fd3182ce40ee08ffeb60b
unknown <PS24CKowalewski@efcts.us>
2023-05-17 08:25:21 -0400

    funky mode now activates properly
    it can only activate once and only happens when you click the button in the navbar labelled "Funky Mode"

--- map.js
+++ map.js
@@ -6,8 +6,6 @@
 
 let map = L.map("map").setView([39.960938, -83.017194], 11);
 
-let funkymode= null;
-
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 19,
   minZoom: 11,
@@ -27,6 +25,8 @@ let testIcon = L.icon({
   shadowAnchor: [4, 20],
   popupAnchor: [-5, -40],
 });
+
+let funkymode = null;
 
 loadJSON();
 
@@ -116,17 +116,18 @@ function fillMap(locValues, SubwayData)
   }
 }
 
-
-addEventListener('click', function Funkymode(){
+const Funky = document.getElementById('Funky');
+Funky.addEventListener('click', function Funkymode(){
+  console.log("funky");
+  testIcon = L.icon({
+    iconUrl: "../Resources/Images/pngegg.png",
+    iconSize: [25, 48],
+    shadowSize: [38, 40],
+    iconAnchor: [19, 30],
+    shadowAnchor: [4, 20],
+    popupAnchor: [-5, -40],
+  });
   for (let j = 0; j < funkymode.businesses.length; ++j) {
-    testIcon = L.icon({
-      iconUrl: "../Resources/Images/pngegg.png",
-      iconSize: [25, 48],
-      shadowSize: [38, 40],
-      iconAnchor: [19, 30],
-      shadowAnchor: [4, 20],
-      popupAnchor: [-5, -40],
-    });
     L.marker(
       [
         funkymode.businesses[j].coordinates.latitude,
@@ -155,4 +156,4 @@ addEventListener('click', function Funkymode(){
           .openOn(map);
       });
   }
-});
+}, {once: true});
