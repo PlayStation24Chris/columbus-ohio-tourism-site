4a3cf1858b569be726bb26f376928fd8c1442105
unknown <PS24CKowalewski@efcts.us>
2023-05-19 10:08:47 -0400

    your honor, the plan is already complete

--- subway.js
+++ subway.js
@@ -3,8 +3,18 @@
 // yelp fusion key
 //1q5Wyuia20sg6oTNXdP3N16DVxyXOgjddjzmQURK9Y1KzVBBFK5VvoQ_XAuamKYG1ZTBImLDYQ0rtVsqgeyCivZeEUGgD_1vc1ozyrmFI2wnBNEUzDnDWQKRce5cZHYx
 
-let subways = null;
+const options = {
+    method: 'GET',
+    headers: {
+        "accept": "application/json",
+        "x-requested-with": "xmlhttprequest",
+        "Access-Control-Allow-Origin": "*",
+        "Authorization": "Bearer 1q5Wyuia20sg6oTNXdP3N16DVxyXOgjddjzmQURK9Y1KzVBBFK5VvoQ_XAuamKYG1ZTBImLDYQ0rtVsqgeyCivZeEUGgD_1vc1ozyrmFI2wnBNEUzDnDWQKRce5cZHYx"
+    }
+};
 
+let subways = null;
+let reviewData = null;
 let endOutput = "";
 
 //get yelp reviews
@@ -12,18 +22,30 @@ importSubways();
 
 async function importSubways()
 {
-    const response = await fetch('../YelpSubway.json');
-    const yelp = await response.json()
+    let rspns = await fetch('../YelpSubway.json');
+    const yelp = await rspns.json()
     subways = yelp.businesses;
     parseInfo();
-    
-    console.log(endOutput.id); //use in API call
     
     const imgEl = document.getElementById('subwayImg');
     if (endOutput.image_url != "")
     {
         imgEl.src = endOutput.image_url;
     }
+
+    console.log(endOutput.id); //use in API call
+    fetch("https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/"+endOutput.id+"/reviews?limit=2&sort_by=yelp_sort", options).then(response => response.json()).then(response => fillReviews(response));
+    /*$.ajax({
+        url: "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/"+endOutput.id+"/reviews",
+        method: 'GET',
+        headers: {
+            'accept': 'application/json',
+            'x-requested-with': 'xmlhttprequest',
+            'Access-Control-Allow-Origin': '*',
+            'Authorization': 'Bearer 1q5Wyuia20sg6oTNXdP3N16DVxyXOgjddjzmQURK9Y1KzVBBFK5VvoQ_XAuamKYG1ZTBImLDYQ0rtVsqgeyCivZeEUGgD_1vc1ozyrmFI2wnBNEUzDnDWQKRce5cZHYx'
+        },
+        success: fillReviews(result)
+    });*/
 }
 
 //import yelp from '../YelpSubway.json' assert { type: 'json' }; //DEPRECATED LMAO
@@ -54,4 +76,41 @@ function sortSubways(loc)
     }
 
     return output;
+}
+
+function fillReviews(data)
+{
+    console.log(data);
+    
+    let rImg = null;
+    let rTxt = null;
+    let rNam = null;
+    let rLnk = null;
+
+    for (let j=0; j<data.reviews.length; ++j)
+    {
+        rImg = document.getElementById('rImg'+j);
+        rTxt = document.getElementById('rText'+j);
+        rNam = document.getElementById('rName'+j);
+        rLnk = document.getElementById('rLink'+j);
+        
+        if (data.reviews[j].user.image_url != null)
+        {
+            rImg.src = data.reviews[j].user.image_url;
+        }
+        else
+        {
+            rImg.src = 'Resources/Images/Bat Walter White.jpg';
+        }
+
+        rTxt.textContent = data.reviews[j].text;
+        rNam.textContent = data.reviews[j].user.name;
+        rLnk.href = data.reviews[j].url;
+    }
+
+    if (data.reviews.length < 2)
+    {
+        let reviewPanels = document.getElementsByClassName('review');
+        reviewPanels[data.reviews.length].style.visibility = 'hidden';
+    }
 }
